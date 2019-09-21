a7ccdf9861460203cebe997a5acc3e6600655f9c
rafal <ravmiecznk@gmail.com>
2019-09-21 12:40:45 +0200

    renamed

--- avr_txmessage_sender.cpp
+++ avr_txmessage_sender.cpp
@@ -6,7 +6,8 @@
  */
 
 
-#include "avr_message_sender.h"
+#include "avr_txmessage_sender.h"
+
 #include "../atm128_usart/usart.h"
 #include "../common.h"
 #include <avr/crc16.h>
@@ -15,7 +16,7 @@
 
 //extern Usart bt_usart;
 
-using namespace MessageSender;
+using namespace tx_id;
 
 
 //int PUTCHAR(char c, FILE* f){
@@ -26,18 +27,25 @@ using namespace MessageSender;
 
 
 //***CONSTRUCTORS***
-Message::Message(uint16_t msg_id, uint16_t context){
+TxMessage::TxMessage(uint16_t msg_id, uint16_t context){
 	tail.id = msg_id;
 	tail.context = context;
 	//_stdout.put	= &PUTCHAR;
 }
 
-Message::~Message(){
+
+TxMessage::TxMessage(){
+	tail.id = tx_id::txt;
+	tail.context = 0;
+	//_stdout.put	= &PUTCHAR;
+}
+
+TxMessage::~TxMessage(){
 	send_tail();
 }
 
 //***METHODS***
-void Message::_putchar(char c){
+void TxMessage::_putchar(char c){
 	usart.Putchar(c);
 }
 
@@ -46,10 +54,10 @@ void Message::_putchar(char c){
 //	return 1;
 //}
 
-void Message::send(char* buffer, uint16_t amount){
+void TxMessage::send(char* buffer, uint16_t amount){
 	for(uint16_t i=0; i<amount; i++){
 		tail.msg_len++;
-		tail.crc = _crc_xmodem_update(tail.crc, *buffer);
+		tail.msg_crc = _crc_xmodem_update(tail.msg_crc, *buffer);
 		usart.Putchar(*buffer);
 		buffer++;				//pointer increment
 	}
@@ -57,55 +65,64 @@ void Message::send(char* buffer, uint16_t amount){
 }
 
 
-void Message::fetch_str(char* buffer){
+void TxMessage::fetch_str(char* buffer){
 	while(*buffer){
 		tail.msg_len++;
-		tail.crc = _crc_xmodem_update(tail.crc, *buffer);
+		tail.msg_crc = _crc_xmodem_update(tail.msg_crc, *buffer);
 		usart.Putchar(*buffer);
 		buffer++;				//pointer increment
 	}
 }
 
-void Message::sends(char* buffer){
+void TxMessage::sends(char* buffer){
 	fetch_str(buffer);
 	send_tail();
 }
 
 
-void Message::fetch_str_p(const char* str_p){
+void TxMessage::fetch_str_p(const char* str_p){
 	char c=pgm_read_byte(str_p++);
 	while(c){
 		tail.msg_len++;
-		tail.crc = _crc_xmodem_update(tail.crc, c);
+		tail.msg_crc = _crc_xmodem_update(tail.msg_crc, c);
 		usart.Putchar(c);
 		c=pgm_read_byte(str_p++);
 	}
 }
 
-void Message::sends_p(const char* buffer){
+void TxMessage::sends_p(const char* buffer){
 	fetch_str_p(buffer);
 	send_tail();
 }
 
-void Message::fetch_byte(uint8_t c){
+void TxMessage::fetch_byte(uint8_t c){
 	usart.Putchar(c);
 	tail.msg_len++;
-	tail.crc = _crc_xmodem_update(tail.crc, c);
+	tail.msg_crc = _crc_xmodem_update(tail.msg_crc, c);
 }
 
-void Message::send_tail(){
+void TxMessage::send_tail(){
 	/*
 	 * Method to close the message by sending its tail
 	 */
+	uint16_t tail_crc = 0;
+	uint8_t* tail_crc_p = (uint8_t*)&tail_crc;
+	char c;
 	if(not tail_was_sent){
 		tail_was_sent = true;
 		uint8_t* p_tail = (uint8_t*)tail;
-		for(uint8_t i=0; i<tail_size; i++)
-			_putchar(*p_tail++);
+		for(uint8_t i=0; i<tail_size; i++){
+			c = *p_tail++;
+			_putchar(c);
+			tail_crc = _crc_xmodem_update(tail_crc, c);
+		}
+		for(uint8_t i=0; i<2; i++){
+			_putchar(tail_crc_p[i]);
+		}
 	}
 }
 
-int Message::printf_fetch(const char* format, ...){
+int TxMessage::printf_fetch(const char* format, ...){
 	/*
 	 * Uses formated print to attach string to message
 	 */
@@ -122,7 +139,7 @@ int Message::printf_fetch(const char* format, ...){
 	return 0;
 }
 
-int Message::printf(const char* format, ...){
+int TxMessage::printf(const char* format, ...){
 	/*
 	 * Sends formatted message
 	 */
@@ -140,7 +157,7 @@ int Message::printf(const char* format, ...){
 }
 
 
-int Message::printf_p_fetch(const char* format, ...){
+int TxMessage::printf_p_fetch(const char* format, ...){
 	/*
 	 * Uses formated print to attach pgmspace string to message
 	 */
@@ -157,7 +174,7 @@ int Message::printf_p_fetch(const char* format, ...){
 	return 0;
 }
 
-int Message::printf_p(const char* format, ...){
+int TxMessage::printf_p(const char* format, ...){
 	/*
 	 * Sends formatted string from pgmspace
 	 */
@@ -175,6 +192,6 @@ int Message::printf_p(const char* format, ...){
 	return 0;
 }
 //***STATIC VARIABLES***
-Usart & Message::usart = get_main_usart();
-uint8_t Message::tail_size = sizeof(Tail);
+Usart & TxMessage::usart = get_main_usart();
+uint8_t TxMessage::tail_size = sizeof(Tail);
 
---
+++ avr_txmessage_sender.h
@@ -0,0 +1,77 @@
+/*
+ * avr_message_sender.h
+ *
+ *  Created on: 11 wrz 2019
+ *      Author: rafal
+ */
+
+#ifndef AVR_MESSAGE_SENDER_AVR_TXMESSAGE_SENDER_H_
+#define AVR_MESSAGE_SENDER_AVR_TXMESSAGE_SENDER_H_
+
+#include <stdint.h>
+#include "../atm128_usart/usart.h"
+
+#define TAIL_START_MARK	'<'
+#define TAIL_END_MARK	'>'
+
+//typedef  int (*putchar_p)(char, FILE*);
+
+int	_putchar_f(char, FILE*);
+typedef int (*putchar_p)(char, FILE*);
+
+namespace tx_id {
+	enum id{
+		ack_feedback,
+		nak_feedback,
+		dtx,
+		txt,
+	};
+}
+
+struct Tail{
+	uint8_t 	tail_start=	TAIL_START_MARK;	//'>'
+	uint16_t	id=			0;
+	uint16_t	context=	0;
+	uint16_t 	msg_len=	0;
+	uint16_t	msg_crc=	0;
+	uint8_t		tail_end=	TAIL_END_MARK;	//'<'
+
+	operator uint8_t* (){
+		return &tail_start;
+	}
+};
+
+class TxMessage{
+	/*
+	 * Class to create and send message.
+	 * |MESSAGE BODY|<TAIL>crc_tail
+	 *
+	 */
+private:
+	void 			_putchar(char);
+	static uint8_t 	tail_size;
+	Tail 			tail;
+	bool			tail_was_sent = false;
+public:
+
+	TxMessage(uint16_t msg_id, uint16_t context);
+	TxMessage();
+	~TxMessage();
+	static 	Usart&	usart;
+	void 	send(char*, uint16_t);
+	void 	sends(char*);
+	void	fetch_str(char*);
+	void 	sends_p(const char *);
+	void 	fetch_str_p(const char *);
+	void 	fetch_byte(uint8_t c);
+	void	send_tail();
+	int 	printf_fetch (const char *format, ...);
+	int 	printf (const char *format, ...);
+	int 	printf_p_fetch (const char *format, ...);
+	int 	printf_p(const char *format, ...);
+};
+
+
+
+
+#endif /* AVR_MESSAGE_SENDER_AVR_TXMESSAGE_SENDER_H_ */
